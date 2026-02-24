d02789c92d4a62e5abc6dbc317d8b454ccc63e6a
Mohd Ayaan Siddiqui <muhammadxayaan007@gmail.com>
2026-02-24 12:49:39 +0530

    initial update

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -31,7 +31,7 @@ const Header = () => {
             </div>
 
             <button id="open-menu" class="md:hidden bg-zinc-900 hover:bg-zinc-800 text-white p-2 rounded-md aspect-square font-medium transition">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <path d="M4 12h16" /><path d="M4 18h16" /><path d="M4 6h16" />
                 </svg>
             </button>
