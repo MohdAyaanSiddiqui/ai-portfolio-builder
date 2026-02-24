5e396ed124adaa8d0b9dd937571b6753123b84c0
Mohd Ayaan Siddiqui <muhammadxayaan007@gmail.com>
2026-02-24 10:59:38 +0530

    Update Footer

--- app/pages/Footer.jsx
+++ app/pages/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
         </div>
 
         <div class="lg:col-span-3 grid grid-cols-2 md:grid-cols-3 gap-8 md:gap-12 lg:gap-28 items-start">
-            <!-- Products -->
+         
             <div>
                 <h3 class="font-medium text-sm mb-4 md:mb-6">Products</h3>
                 <ul class="space-y-3 md:space-y-4 text-sm text-white/70">
@@ -31,7 +31,7 @@ const Footer = () => {
                 </ul>
             </div>
 
-            <!-- Resources -->
+          
             <div>
                 <h3 class="font-medium text-sm mb-4 md:mb-6">Resources</h3>
                 <ul class="space-y-3 md:space-y-4 text-sm text-white/70">
@@ -43,7 +43,7 @@ const Footer = () => {
                 </ul>
             </div>
 
-            <!-- Company -->
+        
             <div class="col-span-2 md:col-span-1">
                 <h3 class="font-medium text-sm mb-4 md:mb-6">Company</h3>
                 <ul class="space-y-3 md:space-y-4 text-sm text-white/70">
@@ -63,31 +63,30 @@ const Footer = () => {
     <div class="max-w-7xl mx-auto mt-12 md:mt-16 pt-6 border-t border-neutral-700 flex flex-col md:flex-row justify-between items-center gap-6">
         <p class="text-white/70 text-xs sm:text-sm order-2 md:order-1">© 2025 prebuiltUI Design</p>
         <div class="flex gap-5 md:gap-6 order-1 md:order-2">
-            <!-- X (Twitter) -->
+    
             <a href="#" class="text-white hover:text-gray-300">
                     <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
                 </svg>
             </a>
-            <!-- Github -->
             <a href="#" class="text-white hover:text-gray-300">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-line-cap="round" stroke-linejoin="round">
                     <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/>
                 </svg>
             </a>
-            <!-- Linkedin -->
+            
             <a href="#" class="text-white hover:text-gray-300">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-line-cap="round" stroke-linejoin="round">
                     <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/>
                 </svg>
             </a>
-            <!-- Youtube -->
+            
             <a href="#" class="text-white hover:text-gray-300">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-line-cap="round" stroke-linejoin="round">
                     <path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17"/><path d="m10 15 5-3-5-3z"/>
                 </svg>
             </a>
-            <!-- Instagram -->
+       
             <a href="#" class="text-white hover:text-gray-300">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-line-cap="round" stroke-linejoin="round"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/></svg>
             </a>
